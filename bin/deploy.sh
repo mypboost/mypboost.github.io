a3b84db2fd1cbb1b3fad369a4e14cb562edc741f
Gerard Burns <gjburns9@hotmail.com>
2017-07-21 01:03:25 +0100

    renamed deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,8 @@ fi
 if [ -d "build" ]; then
   git branch -D deploy
   git checkout -b deploy 
+  git remote remove deploy
+  git remote add deploy git@github.com:mypboost/mypboost.github.io.git
 
   if [[ $(git rev-parse --symbolic-full-name --abbrev-ref HEAD) = deploy ]]; then
     find . ! \( -name "build" -o -name ".git" -o -name ".gitignore" -o -name "node_modules" -o -name "." -o -name ".." \) -maxdepth 1 -exec rm -rf {} +
@@ -14,7 +16,7 @@ if [ -d "build" ]; then
     rm -rf build
     git add .
     git commit -m "build"
-    git push -f
+    git push deploy deploy:master -f
     git checkout master 
     git branch -D deploy
   fi
