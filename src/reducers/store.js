3f3cccdaa4a8d85d2ac2876e908b35108b8cc598
Brian <brian@nicoles-air.home>
2017-09-04 01:26:29 +0100

    build

--- src/reducers/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import { combineReducers } from 'redux-immutable';
-import form from './form';
-import DefaultState from './default-state';
-
-const combinedReducers = combineReducers({form: form}, DefaultState);
-const store = createStore(combinedReducers);
-
-export default store;
--- src/utils/scroll-to.js
+++
@@ -1,6 +0,0 @@
-const scrollTo = (anchor, offset) => {
-  document.getElementById(anchor).scrollIntoView(true);
-  window.scrollBy(0, offset);
-};
-
-export default scrollTo;
